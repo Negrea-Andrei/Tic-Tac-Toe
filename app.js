3378df7f707ff0e7384a5cc9fda51134114feaa7
Negrea-Andrei <negrea97@yahoo.com>
2023-05-27 20:29:57 +0300

    Gameflow

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
-const gameboard = () => {
-    let board = ['', '', '', '', '', '', '', '', '',];
+const gameboard = (() => {
+    let board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
     let container = document.querySelector('.game-board-container');
-    board.forEach(element => {
+    board.forEach((element, index) => {
         let square = document.createElement('div');
+        square.className = "square";
+        square.setAttribute("data-number", index)
         square.style.border = 'solid 1px black'
         container.appendChild(square)
     })
+    return {board}
+})();
+
+const Player = (name) => {
+    return {name}
 }
 
-gameboard()
+const gameflow =(() => {  
+    let squares = document.querySelectorAll('.square');
+    let number = 0;
+    squares.forEach((square, index) => { square.addEventListener('click', () => {        
+        if(number == 0 && square.innerHTML == '') {
+            square.innerHTML = "X";
+            number = 1
+        }
+        else if (number == 1 && square.innerHTML == '') {
+            square.innerHTML = 'O';
+            number = 0;
+        }       
+        })        
+    })
+})();
